4bfa235d7d212c311c6b39bef4f205baf5e370fb
Vansh <99641079+v-code01@users.noreply.github.com>
2023-12-15 12:36:00 -0600

    Update advanced_password_checker.py

--- advanced_password_checker.py
+++ advanced_password_checker.py
@@ -1,1 +1,75 @@
+import re
+import math
+import hashlib
+import string
+import itertools
 
+class SuperAdvancedPasswordChecker:
+    def __init__(self, password):
+        self.password = password
+        self.strength_score = 0
+
+    def check_length(self):
+        length = len(self.password)
+        if length >= 8:
+            self.strength_score += 50
+        elif length >= 12:
+            self.strength_score += 100
+
+    def check_complexity(self):
+        # Check for uppercase, lowercase, digits, and special characters
+        if re.search(r'[A-Z]', self.password) and re.search(r'[a-z]', self.password):
+            self.strength_score += 100
+        if re.search(r'\d', self.password):
+            self.strength_score += 100
+        if re.search(r'[!@#$%^&*(),.?":{}|<>]', self.password):
+            self.strength_score += 100
+
+    def check_entropy(self):
+        # Shannon entropy calculation for password randomness
+        character_set = set(self.password)
+        entropy = -sum((self.password.count(char) / len(self.password)) * math.log2(self.password.count(char) / len(self.password)) for char in character_set)
+        self.strength_score += int(100 - (entropy * 10))
+
+    def check_dictionary_attack(self):
+        # Advanced dictionary attack prevention
+        hashed_password = hashlib.sha256(self.password.encode()).hexdigest()
+        # Check against a secure hash of common passwords
+        common_password_hashes = ["5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "3efbe02bb1fb582bca47b85e6374042e"]
+        if hashed_password not in common_password_hashes:
+            self.strength_score += 100
+
+    def check_common_patterns(self):
+        # Advanced common patterns check
+        for pattern in ["123", "password", "qwerty"]:
+            if pattern in self.password.lower():
+                self.strength_score -= 50
+
+    def check_bruteforce_resistance(self):
+        # Advanced bruteforce resistance check
+        charset = string.ascii_letters + string.digits + string.punctuation
+        total_possible_passwords = len(charset) ** len(self.password)
+        bruteforce_resistance = math.log2(total_possible_passwords)
+        if bruteforce_resistance > 80:  # Threshold for high resistance
+            self.strength_score += 100
+
+    def check_statistical_bias(self):
+        # Advanced statistical bias check
+        character_probabilities = {char: self.password.count(char) / len(self.password) for char in set(self.password)}
+        bias_score = -sum(prob * math.log2(prob) for prob in character_probabilities.values())
+        self.strength_score += int(50 - (bias_score * 10))
+
+    def calculate_strength_score(self):
+        # Run all checks to calculate the overall strength score
+        self.check_length()
+        self.check_complexity()
+        self.check_entropy()
+        self.check_dictionary_attack()
+        self.check_common_patterns()
+        self.check_bruteforce_resistance()
+        self.check_statistical_bias()
+
+    def get_security_score(self):
+        # Calculate the overall security score out of 1000
+        self.calculate_strength_score()
+        return round(self.strength_score / 5, 2)  # Scale to a 1000-point system
